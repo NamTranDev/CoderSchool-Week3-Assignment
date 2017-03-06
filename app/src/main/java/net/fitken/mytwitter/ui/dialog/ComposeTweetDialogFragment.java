911e2d2c3bbf0644f34e5a10578929ca0b02f938
Hung Nguyen <hung.nguyen@saigontechnology.vn>
2017-03-06 18:27:27 +0700

    fix UI, update readme

--- app/src/main/java/net/fitken/mytwitter/ui/dialog/ComposeTweetDialogFragment.java
+++ app/src/main/java/net/fitken/mytwitter/ui/dialog/ComposeTweetDialogFragment.java
@@ -1,6 +1,9 @@
 package net.fitken.mytwitter.ui.dialog;
 
+import android.app.Dialog;
 import android.databinding.DataBindingUtil;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
@@ -66,6 +69,16 @@ public class ComposeTweetDialogFragment extends DialogFragment {
 
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        Dialog dialog = getDialog();
+        if (dialog != null) {
+            dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            dialog.setCancelable(false);
+        }
+    }
+
     @OnClick(R.id.img_close)
     public void close(View view) {
         dismiss();
@@ -76,7 +89,6 @@ public class ComposeTweetDialogFragment extends DialogFragment {
         MyApplication.getRestClient().postTweet(binding.etTweetContent.getText().toString(), new AsyncHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-                AlertDialogUtils.showError(getActivity(), "success");
                 dismiss();
                 if (handler != null) {
                     handler.onPostedNewTweet();
