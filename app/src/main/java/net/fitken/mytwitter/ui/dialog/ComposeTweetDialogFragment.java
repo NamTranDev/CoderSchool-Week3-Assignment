3e4f1edc8e92cc26826fcfbedd607b77d5c9e373
Hung Nguyen <hung.nguyen@saigontechnology.vn>
2017-03-06 19:33:01 +0700

    apply scroll toolbar, update readme

--- app/src/main/java/net/fitken/mytwitter/ui/dialog/ComposeTweetDialogFragment.java
+++ app/src/main/java/net/fitken/mytwitter/ui/dialog/ComposeTweetDialogFragment.java
@@ -7,10 +7,13 @@ import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.view.WindowManager;
 
 import com.loopj.android.http.AsyncHttpResponseHandler;
 
@@ -30,6 +33,7 @@ import cz.msebera.android.httpclient.Header;
 
 public class ComposeTweetDialogFragment extends DialogFragment {
     public static final String IMG_URL_PROFILE = "imgUrlProfile";
+    public static final int MAX_LENGTH = 140;
     private FragmentComposeTweetBinding binding;
     private MainActivity.ComposeTweet handler;
 
@@ -59,14 +63,31 @@ public class ComposeTweetDialogFragment extends DialogFragment {
         ButterKnife.bind(this, binding.getRoot());
         getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE);
         binding.setImgUrlProfile(getArguments().getString(IMG_URL_PROFILE));
-        getDialog().setCancelable(false);
+        getDialog().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
         return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        binding.etTweetContent.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                int lengthTyped = MAX_LENGTH - s.length();
+                binding.tvCounter.setText(String.valueOf(lengthTyped));
+                binding.btnSubmitTweet.setEnabled(lengthTyped >= 0 && lengthTyped < MAX_LENGTH);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 
     @Override
@@ -74,7 +95,8 @@ public class ComposeTweetDialogFragment extends DialogFragment {
         super.onStart();
         Dialog dialog = getDialog();
         if (dialog != null) {
-            dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
             dialog.setCancelable(false);
         }
     }
